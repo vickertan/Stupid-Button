cea63bcd97e42ae0929738488dd04cbe23845569
VickerTan <vickerpangestan@gmail.com>
2023-03-27 19:35:28 +0800

    Optimize moving range on new size

--- src/tasks/moveButton.js
+++ src/tasks/moveButton.js
@@ -1,8 +1,8 @@
 const moveButton = (e) => {
     const button = document.getElementById(e.currentTarget.id);
 
-    const x_axis = Math.floor(Math.random() * 480 - 90);
-    const y_axis = Math.floor(Math.random() * 190 - 20);
+    const x_axis = Math.floor(Math.random() * 500 - 100);
+    const y_axis = Math.floor(Math.random() * 198 - 30);
 
     button.style.marginRight = x_axis + "px";
     button.style.marginBottom = y_axis + "px";
