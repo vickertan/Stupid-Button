ee8d00fb53235f78c10f0e0596ac418777e973b3
VickerTan <vickerpangestan@gmail.com>
2023-03-27 19:25:16 +0800

    Optimize moving range

--- src/tasks/moveButton.js
+++ src/tasks/moveButton.js
@@ -1,11 +1,13 @@
 const moveButton = (e) => {
     const button = document.getElementById(e.currentTarget.id);
 
-    const x_axis = Math.floor(Math.random() * 450);
-    const y_axis = Math.floor(Math.random() * 200);
+    const x_axis = Math.floor(Math.random() * 480 - 90);
+    const y_axis = Math.floor(Math.random() * 190 - 20);
 
     button.style.marginRight = x_axis + "px";
     button.style.marginBottom = y_axis + "px";
+
+    console.log(x_axis, y_axis);
 };
 
 export default moveButton;
